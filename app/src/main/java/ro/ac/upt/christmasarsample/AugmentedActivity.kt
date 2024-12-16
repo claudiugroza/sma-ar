046d07fcb1300926f65240edc4847f3c93993e9e
Claudiu Groza <claudiu.groza@ascentcore.com>
2024-12-16 18:55:43 +0200

    Bump gradle and rephrase todos

--- app/src/main/java/ro/ac/upt/christmasarsample/AugmentedActivity.kt
+++ app/src/main/java/ro/ac/upt/christmasarsample/AugmentedActivity.kt
@@ -1,17 +1,11 @@
 package ro.ac.upt.christmasarsample
 
-import android.app.Activity
-import android.app.ActivityManager
-import android.content.Context
 import android.net.Uri
-import android.os.Build
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+import android.support.v7.app.AppCompatActivity
 import com.google.ar.core.Anchor
-import com.google.ar.core.HitResult
 import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.NodeParent
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.ux.ArFragment
@@ -32,23 +26,27 @@ class AugmentedActivity : AppCompatActivity() {
 
         arFragment = supportFragmentManager.findFragmentById(R.id.scf_central) as ArFragment
 
-        // TODO("2. Invoke addRenderableToScene once a tap is executed over the AR plane")
-
+        // TODO 2: Invoke addRenderableToScene() once a tap is executed over the AR plane
     }
 
     private fun initRenderableModel() {
         val modelUri = Uri.parse("model.sfb")
 
-        // TODO("1. Init model's renderable variable")
+        // TODO 1: Init model's renderable variable
 
     }
 
-    private fun addRenderableToScene(anchor: Anchor, renderable: Renderable) {
-        // TODO("3. Build an anchor node and set the AR scene to be its parent")
+    private fun addRenderableToScene(anchor: Anchor) {
+        renderable?.let {
+            // TODO 3: Build an anchor node and set the AR scene to be its parent
 
-        // TODO("4. Build an transformable node and set the previously anchor node to be its parent")
 
-        // TODO("5. Assign node's renderable property to previously loaded renderable")
+            // TODO 4: Build an transformable node and set the previously anchor node to be its parent
+
+
+            // TODO 5: Assign node's renderable property to previously loaded renderable
+
+        }
     }
 
     companion object {
